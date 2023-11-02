1cdf3fe66574f3431f2914101f070c8ee88703ea
Prathibha Ratnayake <prsthibha@gmail.com>
2023-11-02 08:44:34 +0530

    update base URL

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create an instance of axios
 const api = axios.create({
   // baseURL: 'http://52.41.255.9:5000/api',
-  baseURL: 'api',
+  baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
     // 'Content-Type': 'multipart/form-data',
