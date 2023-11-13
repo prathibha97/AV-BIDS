d5f7342fed694a07a0121197a466ceb182b099ab
Prathibha Ratnayake <prsthibha@gmail.com>
2023-11-13 16:37:09 +0530

    add file upload and select options

---
+++ client/src/constants/index.tsx
@@ -0,0 +1,78 @@
+export const usStates = [
+  { label: 'Alabama', value: 'Alabama' },
+  { label: 'Alaska', value: 'Alaska' },
+  { label: 'Arizona', value: 'Arizona' },
+  { label: 'Arkansas', value: 'Arkansas' },
+  { label: 'California', value: 'California' },
+  { label: 'Colorado', value: 'Colorado' },
+  { label: 'Connecticut', value: 'Connecticut' },
+  { label: 'Delaware', value: 'Delaware' },
+  { label: 'Florida', value: 'Florida' },
+  { label: 'Georgia', value: 'Georgia' },
+  { label: 'Hawaii', value: 'Hawaii' },
+  { label: 'Idaho', value: 'Idaho' },
+  { label: 'Illinois', value: 'Illinois' },
+  { label: 'Indiana', value: 'Indiana' },
+  { label: 'Iowa', value: 'Iowa' },
+  { label: 'Kansas', value: 'Kansas' },
+  { label: 'Kentucky', value: 'Kentucky' },
+  { label: 'Louisiana', value: 'Louisiana' },
+  { label: 'Maine', value: 'Maine' },
+  { label: 'Maryland', value: 'Maryland' },
+  { label: 'Massachusetts', value: 'Massachusetts' },
+  { label: 'Michigan', value: 'Michigan' },
+  { label: 'Minnesota', value: 'Minnesota' },
+  { label: 'Mississippi', value: 'Mississippi' },
+  { label: 'Missouri', value: 'Missouri' },
+  { label: 'Montana', value: 'Montana' },
+  { label: 'Nebraska', value: 'Nebraska' },
+  { label: 'Nevada', value: 'Nevada' },
+  { label: 'New Hampshire', value: 'New Hampshire' },
+  { label: 'New Jersey', value: 'New Jersey' },
+  { label: 'New Mexico', value: 'New Mexico' },
+  { label: 'New York', value: 'New York' },
+  { label: 'North Carolina', value: 'North Carolina' },
+  { label: 'North Dakota', value: 'North Dakota' },
+  { label: 'Ohio', value: 'Ohio' },
+  { label: 'Oklahoma', value: 'Oklahoma' },
+  { label: 'Oregon', value: 'Oregon' },
+  { label: 'Pennsylvania', value: 'Pennsylvania' },
+  { label: 'Rhode Island', value: 'Rhode Island' },
+  { label: 'South Carolina', value: 'South Carolina' },
+  { label: 'South Dakota', value: 'South Dakota' },
+  { label: 'Tennessee', value: 'Tennessee' },
+  { label: 'Texas', value: 'Texas' },
+  { label: 'Utah', value: 'Utah' },
+  { label: 'Vermont', value: 'Vermont' },
+  { label: 'Virginia', value: 'Virginia' },
+  { label: 'Washington', value: 'Washington' },
+  { label: 'West Virginia', value: 'West Virginia' },
+  { label: 'Wisconsin', value: 'Wisconsin' },
+  { label: 'Wyoming', value: 'Wyoming' },
+] as const;
+
+export const eventBudgetOptions = [
+  { label: '<$70,000', value: '<70000' },
+  { label: '$70,000 - $150,000', value: '70000-150000' },
+  { label: '$150,000 - $500,000', value: '150000-500000' },
+  { label: '$500,000 - $1,000,000', value: '500000-1000000' },
+  { label: '$1,000,000+', value: '1000000+' },
+]as const;
+
+export const eventTypes = [
+  { label: 'In-Person', value: 'In-Person' },
+  { label: 'Virtual', value: 'Virtual' },
+  { label: 'Hybrid', value: 'Hybrid' },
+] as const;
+
+export const eventCategories = [
+  { label: 'Corporate', value: 'Corporate' },
+  { label: 'Non-Corporate', value: 'Non-Corporate' },
+]as const;
+
+export const eventSubCategories = [
+  { label: 'Awards', value: 'Awards' },
+  { label: 'Banquet', value: 'Banquet' },
+  { label: 'Board Meeting', value: 'Board Meeting' },
+  { label: 'Breakout Session', value: 'Breakout Session' },
+]as const;
--- client/src/pages/13_edit_event/components/step-one.tsx
+++ client/src/pages/13_edit_event/components/step-one.tsx
@@ -1,9 +1,25 @@
 import { Button, Input, Option, Select } from '@material-tailwind/react';
+import axios from 'axios';
 import { FC, useState } from 'react';
 import { UseFormRegister, UseFormReturn } from 'react-hook-form';
 import DELETE_BUTTON from '../../../assets/12_edit_event/ep_delete.png';
+import {
+  eventBudgetOptions,
+  eventCategories,
+  eventSubCategories,
+  eventTypes,
+  usStates,
+} from '../../../constants';
+import api from '../../../utils/api';
 import { EventFormFormValues } from '../../../utils/validations/event-form-validation';
 import RichTextEditor from './rich-text-editor';
+
+interface UploadedFile extends File {
+  fileName: string;
+  url?: string; // Set to optional as it will be updated after upload
+  progress: number;
+}
+
 
 interface StepOneProps {
   control: UseFormReturn<any>['control'];
@@ -12,18 +28,86 @@ interface StepOneProps {
 }
 
 const StepOne: FC<StepOneProps> = ({ control, register, updateFormData }) => {
-  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [uploadedFiles, setUploadedFiles] = useState<UploadedFile[]>([]);
 
-  const handleFileUpload = (e: any) => {
+  // const handleFileUpload = (e: any) => {
+  //   const files = e.target.files;
+  //   const updatedFiles = [...uploadedFiles];
+
+  //   for (let i = 0; i < files.length; i++) {
+  //     // @ts-ignore
+  //     updatedFiles.push(files[i]);
+  //   }
+
+  //   setUploadedFiles(updatedFiles);
+  // };
+
+  const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
-    const updatedFiles = [...uploadedFiles];
-
-    for (let i = 0; i < files.length; i++) {
+    if (files) {
+      const updatedFiles = Array.from(files);
       // @ts-ignore
-      updatedFiles.push(files[i]);
-    }
+      setUploadedFiles((prevFiles) => [...prevFiles, ...updatedFiles]);
 
-    setUploadedFiles(updatedFiles);
+      // Iterate over the newly added files and update formData
+      for (let i = 0; i < updatedFiles.length; i++) {
+        await uploadFileToS3(files[i], i);
+      }
+    }
+  };
+
+  const uploadFileToS3 = async (file: File, index: number) => {
+    // @ts-ignore
+    const fileExtension = file.name.split('.').pop().toLowerCase();
+
+    if (fileExtension) {
+      try {
+        // Get S3 upload config
+        const uploadConfig = await api.get('/upload?type=' + fileExtension);
+
+        // Upload file to S3
+        await axios.put(uploadConfig.data.url, file, {
+          headers: {
+            'Content-Type': file.type,
+          },
+          onUploadProgress: (progressEvent) => {
+            const percentCompleted = Math.round(
+              // @ts-ignore
+              (progressEvent.loaded * 100) / progressEvent.total
+            );
+
+            setUploadedFiles((prevFiles) =>
+              prevFiles.map((prevFile, i) =>
+                i === index
+                  ? { ...prevFile, progress: percentCompleted }
+                  : prevFile
+              )
+            );
+          },
+        });
+
+        // Update form data or do any other actions with the uploaded file info
+        updateFormData((prevData: any) => ({
+          ...prevData,
+          // Add logic to store file info as needed
+          files: [
+            ...(prevData.files || []), // Previous files, if any
+            {
+              fileName: file.name,
+              url: uploadConfig.data.key,
+            },
+          ],
+        }));
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setUploadedFiles((prevFiles) =>
+          prevFiles.map((prevFile, i) =>
+            i === index ? { ...prevFile, progress: 0 } : prevFile
+          )
+        );
+      }
+    }
   };
 
   const handleInputChange = (name: string, value: string) => {
@@ -81,11 +165,11 @@ const StepOne: FC<StepOneProps> = ({ control, register, updateFormData }) => {
                   handleSelect('eventType', e);
                 }}
               >
-                <Option value='Option 01'>Option 01</Option>
-                <Option value='Option 02'>Option 02</Option>
-                <Option value='Option 03'>Option 03</Option>
-                <Option value='Option 04'>Option 04</Option>
-                <Option value='Option 05'>Option 05</Option>
+                {eventTypes.map((type) => (
+                  <Option key={type.value} value={type.value}>
+                    {type.label}
+                  </Option>
+                ))}
               </Select>
             </div>
           </div>
@@ -100,11 +184,11 @@ const StepOne: FC<StepOneProps> = ({ control, register, updateFormData }) => {
                   handleSelect('eventCategory', e);
                 }}
               >
-                <Option value='Option 01'>Option 01</Option>
-                <Option value='Option 02'>Option 02</Option>
-                <Option value='Option 03'>Option 03</Option>
-                <Option value='Option 04'>Option 04</Option>
-                <Option value='Option 05'>Option 05</Option>
+                {eventCategories.map((category) => (
+                  <Option key={category.value} value={category.value}>
+                    {category.label}
+                  </Option>
+                ))}
               </Select>
             </div>
           </div>
@@ -119,11 +203,11 @@ const StepOne: FC<StepOneProps> = ({ control, register, updateFormData }) => {
                   handleSelect('eventSubCategory', e);
                 }}
               >
-                <Option value='Option 01'>Option 01</Option>
-                <Option value='Option 02'>Option 02</Option>
-                <Option value='Option 03'>Option 03</Option>
-                <Option value='Option 04'>Option 04</Option>
-                <Option value='Option 05'>Option 05</Option>
+                {eventSubCategories.map((category) => (
+                  <Option key={category.value} value={category.value}>
+                    {category.label}
+                  </Option>
+                ))}
               </Select>
             </div>
           </div>
@@ -138,11 +222,11 @@ const StepOne: FC<StepOneProps> = ({ control, register, updateFormData }) => {
                   handleSelect('eventBudget', e);
                 }}
               >
-                <Option value='Option 01'>Option 01</Option>
-                <Option value='Option 02'>Option 02</Option>
-                <Option value='Option 03'>Option 03</Option>
-                <Option value='Option 04'>Option 04</Option>
-                <Option value='Option 05'>Option 05</Option>
+                {eventBudgetOptions.map((budget) => (
+                  <Option key={budget.value} value={budget.value}>
+                    {budget.label}
+                  </Option>
+                ))}
               </Select>
             </div>
           </div>
@@ -209,11 +293,11 @@ const StepOne: FC<StepOneProps> = ({ control, register, updateFormData }) => {
                   handleSelect('address.state', e);
                 }}
               >
-                <Option value='Option 01'>Option 01</Option>
-                <Option value='Option 02'>Option 02</Option>
-                <Option value='Option 03'>Option 03</Option>
-                <Option value='Option 04'>Option 04</Option>
-                <Option value='Option 05'>Option 05</Option>
+                {usStates.map((state) => (
+                  <Option key={state.value} value={state.value}>
+                    {state.label}
+                  </Option>
+                ))}
               </Select>
             </div>
           </div>
@@ -239,14 +323,30 @@ const StepOne: FC<StepOneProps> = ({ control, register, updateFormData }) => {
         {uploadedFiles.length > 0 ? (
           <div className='grid grid-cols-2 gap-x-16 gap-y-4 font-medium text-[18px] text-black mb-2'>
             {uploadedFiles.map((file, index) => (
-              <div key={index} className='flex items-center gap-24'>
-                {/* @ts-ignore */}
-                <p>{file.name}</p>
-                <img
-                  src={DELETE_BUTTON}
-                  alt='delete'
-                  className='object-scale-down w-[34px]'
-                />
+              <div key={index} className='mb-4'>
+                <div className='flex items-center gap-4'>
+                  <p>{file.name}</p>
+                  <img
+                    src={DELETE_BUTTON}
+                    alt='delete'
+                    className='object-scale-down w-[34px]'
+                  />
+                </div>
+                <div className='relative pt-1'>
+                  <div className='flex mb-2 items-center justify-between'>
+                    <div>
+                      <span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-teal-600 bg-teal-200'>
+                        {file.progress}%
+                      </span>
+                    </div>
+                  </div>
+                  <div className='flex h-2 mb-4 overflow-hidden bg-gray-200 rounded'>
+                    <div
+                      style={{ width: `${file.progress}%` }}
+                      className='shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500'
+                    ></div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
@@ -267,6 +367,7 @@ const StepOne: FC<StepOneProps> = ({ control, register, updateFormData }) => {
               accept='.pdf, .doc, .docx, .jpg, .jpeg, .png'
               onChange={handleFileUpload}
               className='hidden' // This hides the file input element
+              multiple
             />
           </label>
         </Button>
