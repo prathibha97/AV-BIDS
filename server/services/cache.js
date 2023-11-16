891163115727cf5b235084a14473b962566bbe63
Prathibha Ratnayake <prsthibha@gmail.com>
2023-11-16 19:35:59 +0530

    bug fix

--- server/services/cache.js
+++ server/services/cache.js
@@ -3,7 +3,7 @@ const redis = require("redis");
 const util = require("util");
 const keys = require("../config/keys");
 
-const client = redis.createClient('redis://redis:6379');
+const client = redis.createClient(keys.redisUrl);
 
 // use promisify to aviod using callbacks
 client.hget = util.promisify(client.hget);
