f9c2a5bd6cfd55f31ea7ae13ebb9b0d4fbac2718
diduladishan <diduladishan14@gmail.com>
2023-11-20 15:26:18 +0530

    stepper added

---
+++ client/src/pages/events/edit/components/stepper.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Stepper, Step, Button } from "@material-tailwind/react";
+
+function DefaultStepper() {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [isLastStep, setIsLastStep] = React.useState(false);
+  const [isFirstStep, setIsFirstStep] = React.useState(false);
+
+  const handleNext = () => !isLastStep && setActiveStep((cur) => cur + 1);
+  const handlePrev = () => !isFirstStep && setActiveStep((cur) => cur - 1);
+
+  return (
+    <div className="w-full py-4 px-8">
+      <Stepper
+        activeStep={activeStep}
+        isLastStep={(value) => setIsLastStep(value)}
+        isFirstStep={(value) => setIsFirstStep(value)}
+      >
+        <Step onClick={() => setActiveStep(0)} className="!bg-[#42D27A]">
+          <span>1</span>
+        </Step>
+
+        <Step
+          onClick={() => setActiveStep(1)}
+          className={activeStep === 1 ? "active-step" : ""}
+        >
+          <span>2</span>
+        </Step>
+        {/* <Step
+          onClick={() => setActiveStep(2)}
+          className={activeStep === 2 ? "active-step" : ""}
+        >
+          <span>3</span>
+        </Step> */}
+      </Stepper>
+
+      <div className="w-full flex justify-between"></div>
+
+      {/* <div className="mt-16 flex justify-between">
+        <Button onClick={handlePrev} disabled={isFirstStep}>
+          Prev
+        </Button>
+        <Button onClick={handleNext} disabled={isLastStep}>
+          Next
+        </Button>
+      </div> */}
+      <style>
+        {`.active-step {
+          background-color: #42D27A; /* Change this to the desired color */
+        }`}
+      </style>
+    </div>
+  );
+}
+
+export default DefaultStepper;
--- client/src/pages/events/edit/index.tsx
+++ client/src/pages/events/edit/index.tsx
@@ -1,23 +1,24 @@
-import { Button } from '@material-tailwind/react';
+import { Button } from "@material-tailwind/react";
 
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useAppSelector } from '../../../app/hooks';
-import { RootState } from '../../../app/store';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useAppSelector } from "../../../app/hooks";
+import { RootState } from "../../../app/store";
 import {
   EventFormFormValues,
   EventFormSchema,
-} from '../../../utils/validations/event-form-validation';
-import StepFive from './components/step-five';
-import StepFour from './components/step-four';
-import StepOne from './components/step-one';
-import StepSeven from './components/step-seven';
-import StepSix from './components/step-six';
-import StepThree from './components/step-three';
-import StepTwo from './components/step-two';
-import api from '../../../utils/api';
-import { useNavigate } from 'react-router-dom';
+} from "../../../utils/validations/event-form-validation";
+import StepFive from "./components/step-five";
+import StepFour from "./components/step-four";
+import StepOne from "./components/step-one";
+import StepSeven from "./components/step-seven";
+import StepSix from "./components/step-six";
+import StepThree from "./components/step-three";
+import StepTwo from "./components/step-two";
+import api from "../../../utils/api";
+import { useNavigate } from "react-router-dom";
+import Stepper from "./components/stepper";
 
 interface Requirement {
   label: string;
@@ -25,7 +26,7 @@ interface Requirement {
 }
 
 export function Index() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [currentStep, setCurrentStep] = useState(1);
 
   const event = useAppSelector((state: RootState) => state.event.event);
@@ -48,7 +49,7 @@ export function Index() {
       await api.put(`/events/${event?._id}`, {
         ...formData,
       });
-      navigate('/events/my-events');
+      navigate("/events/my-events");
     } catch (error) {
       console.log(error);
     }
@@ -58,7 +59,7 @@ export function Index() {
     // Populate formData with event data when component mounts
     if (event) {
       setFormData({
-        ...event
+        ...event,
       });
     }
   }, [event]);
@@ -77,25 +78,26 @@ export function Index() {
     }));
   };
 
-
   return (
-    <div className='container mx-auto mb-8'>
-      <section className='bg-white px-8 py-8 rounded-xl drop-shadow-md'>
-        <h2 className='text-[20px] font-semibold mb-6'>Edit Event</h2>
+    <div className="container mx-auto mb-8">
+      <section className="bg-white px-8 py-8 rounded-xl drop-shadow-md">
+        <h2 className="text-[20px] font-semibold mb-6">Edit Event</h2>
 
-        <div className='grid grid-cols-2 mb-8'>
-          <div className='flex flex-col items-center gap-2'>
-            <div className='flex  items-center justify-center rounded-full w-10 h-10 bg-[#42D27A] text-white'>
-              <p>1</p>
+        <div className="flex justify-center mb-10">
+          <div>
+            <div className="mx-5">
+              <Stepper />
             </div>
 
-            <p className='font-medium text-[16px]'>Update Information</p>
-          </div>
-          <div className='flex flex-col items-center gap-2'>
-            <div className='flex  items-center justify-center rounded-full w-10 h-10 bg-[#42D27A] text-white'>
-              <p>2</p>
+            <div className="flex items-center justify-between gap-16">
+              <div>
+                <p className="font-medium text-[16px]">Update Information </p>
+              </div>
+
+              <div>
+                <p className="text-[16px] font-medium ">Update Settings</p>
+              </div>
             </div>
-            <p className='text-[16px] font-medium '>Equipment Requirements</p>
           </div>
         </div>
 
@@ -167,59 +169,59 @@ export function Index() {
           <StepSeven formData={formData} updateFormData={updateFormData} />
         )}
 
-        <div className='flex items-center justify-between mt-6'>
+        <div className="flex items-center justify-between mt-6">
           <div>
             <Button
-              variant='outlined'
-              size='sm'
-              className='rounded-full  py-3 px-6 mt-4 bg-[#EBEBEB] font-poppins normal-case border-none w-[135px]'
+              variant="outlined"
+              size="sm"
+              className="rounded-full  py-3 px-6 mt-4 bg-[#EBEBEB] font-poppins normal-case border-none w-[135px]"
               onClick={() => setCurrentStep(1)}
             >
-              <span className='text-black'>Cancel</span>
+              <span className="text-black">Cancel</span>
             </Button>
           </div>
 
           <div>
             <Button
-              variant='outlined'
-              color='indigo'
-              size='sm'
-              className='rounded-full  py-3 px-6 mt-4  font-poppins normal-case border-primary w-[135px] mr-6'
+              variant="outlined"
+              color="indigo"
+              size="sm"
+              className="rounded-full  py-3 px-6 mt-4  font-poppins normal-case border-primary w-[135px] mr-6"
             >
-              <span className='text-primary '>Save as Draft</span>
+              <span className="text-primary ">Save as Draft</span>
             </Button>
 
             {currentStep > 1 && (
               <Button
-                variant='outlined'
-                size='sm'
-                className='rounded-full py-3 px-6 mt-4 bg-[#EBEBEB] font-poppins normal-case border-none w-[135px] mr-4'
+                variant="outlined"
+                size="sm"
+                className="rounded-full py-3 px-6 mt-4 bg-[#EBEBEB] font-poppins normal-case border-none w-[135px] mr-4"
                 onClick={handlePrevStep}
               >
-                <span className='text-black'>Previous</span>
+                <span className="text-black">Previous</span>
               </Button>
             )}
 
             {currentStep < 7 ? (
               <Button
-                variant='filled'
-                color='indigo'
-                size='sm'
-                className='rounded-full py-3 px-6 mt-4 font-poppins normal-case bg-primary w-[135px]'
+                variant="filled"
+                color="indigo"
+                size="sm"
+                className="rounded-full py-3 px-6 mt-4 font-poppins normal-case bg-primary w-[135px]"
                 onClick={handleNextStep}
               >
-                <span className='text-white'>Next</span>
+                <span className="text-white">Next</span>
               </Button>
             ) : (
               <Button
-                variant='filled'
-                color='indigo'
-                size='sm'
-                className='rounded-full py-3 px-6 mt-4 font-poppins normal-case bg-primary w-[135px]'
-                type='button'
+                variant="filled"
+                color="indigo"
+                size="sm"
+                className="rounded-full py-3 px-6 mt-4 font-poppins normal-case bg-primary w-[135px]"
+                type="button"
                 onClick={onSubmit}
               >
-                <span className='text-white'>Submit</span>
+                <span className="text-white">Submit</span>
               </Button>
             )}
           </div>
