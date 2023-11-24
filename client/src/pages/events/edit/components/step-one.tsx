88045ce77ab485beca695faf01d8d207fa9ef733
diduladishan <diduladishan14@gmail.com>
2023-11-24 15:13:38 +0530

    UI updated

--- client/src/pages/events/edit/components/step-one.tsx
+++ client/src/pages/events/edit/components/step-one.tsx
@@ -1,18 +1,18 @@
-import { Button, Input, Option, Select } from '@material-tailwind/react';
-import axios from 'axios';
-import { FC, useState } from 'react';
-import { UseFormRegister, UseFormReturn } from 'react-hook-form';
-import DELETE_BUTTON from '../../../../assets/12_edit_event/ep_delete.png';
+import { Button, Input, Option, Select } from "@material-tailwind/react";
+import axios from "axios";
+import { FC, useState } from "react";
+import { UseFormRegister, UseFormReturn } from "react-hook-form";
+import DELETE_BUTTON from "../../../../assets/12_edit_event/ep_delete.png";
 import {
   eventBudgetOptions,
   eventCategories,
   eventSubCategories,
   eventTypes,
   usStates,
-} from '../../../../constants';
-import api from '../../../../utils/api';
-import { EventFormFormValues } from '../../../../utils/validations/event-form-validation';
-import RichTextEditor from './rich-text-editor';
+} from "../../../../constants";
+import api from "../../../../utils/api";
+import { EventFormFormValues } from "../../../../utils/validations/event-form-validation";
+import RichTextEditor from "./rich-text-editor";
 
 interface UploadedFile extends File {
   fileName: string;
@@ -21,7 +21,7 @@ interface UploadedFile extends File {
 }
 
 interface StepOneProps {
-  control: UseFormReturn<any>['control'];
+  control: UseFormReturn<any>["control"];
   register: UseFormRegister<EventFormFormValues>;
   updateFormData: any;
   formData: any;
@@ -51,17 +51,17 @@ const StepOne: FC<StepOneProps> = ({
 
   const uploadFileToS3 = async (file: File, index: number) => {
     // @ts-ignore
-    const fileExtension = file.name.split('.').pop().toLowerCase();
+    const fileExtension = file.name.split(".").pop().toLowerCase();
 
     if (fileExtension) {
       try {
         // Get S3 upload config
-        const uploadConfig = await api.get('/upload?type=' + fileExtension);
+        const uploadConfig = await api.get("/upload?type=" + fileExtension);
 
         // Upload file to S3
         await axios.put(uploadConfig.data.url, file, {
           headers: {
-            'Content-Type': file.type,
+            "Content-Type": file.type,
           },
           onUploadProgress: (progressEvent) => {
             const percentCompleted = Math.round(
@@ -119,28 +119,32 @@ const StepOne: FC<StepOneProps> = ({
 
   return (
     <div>
-      <p className='font-medium text-[18px] mb-4'>Event Details</p>
+      <p className="font-medium text-[18px] mb-4">Event Details</p>
 
       <form>
-        <div className='grid grid-cols-2 gap-x-16 gap-y-4 font-medium text-[16px] text-[#353535]'>
-          <div className='col-span-2'>
-            <p className='mb-2'>Event Title </p>
-            <div className='mb-5'>
+        <div className="grid grid-cols-2 gap-x-16 gap-y-4 font-medium text-[16px] text-[#353535]">
+          <div className="col-span-2">
+            <p className="mb-2">Event Title </p>
+            <div className="mb-5 bg-input_background rounded-full">
               <Input
-                label='Ex: 2023 Meeting Expo San Deigo'
-                crossOrigin=''
-                className='rounded-md bg-[#eeebfa]'
-                {...register('title')}
+                placeholder="Ex: 2023 Meeting Expo San Deigo"
+                crossOrigin=""
+                className="rounded-full !border !border-gray-300 text-gray-900 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-full"
+                labelProps={{
+                  className: "hidden",
+                }}
+                containerProps={{ className: "min-w-[100px]" }}
+                {...register("title")}
                 onChange={(e: any) => {
-                  handleInputChange('title', e.target.value);
+                  handleInputChange("title", e.target.value);
                 }}
                 value={formData.title}
               />
             </div>
           </div>
-          <div className='col-span-2'>
-            <p className='mb-2'>Event Description </p>
-            <div className='mb-5'>
+          <div className="col-span-2">
+            <p className="mb-2">Event Description </p>
+            <div className="mb-5">
               {/* @ts-ignore */}
               <RichTextEditor
                 control={control}
@@ -149,15 +153,15 @@ const StepOne: FC<StepOneProps> = ({
               />
             </div>
           </div>
-          <div className=''>
-            <p className='mb-2'>Event Type </p>
-            <div className='mb-5'>
+          <div className="">
+            <p className="mb-2">Event Type </p>
+            <div className="mb-5">
               <Select
-                label='In-Person'
-                className='bg-[#eeebfc]'
-                name='eventType'
+                label="In-Person"
+                className="!bg-input_background"
+                name="eventType"
                 onChange={(e: any) => {
-                  handleSelect('eventType', e);
+                  handleSelect("eventType", e);
                 }}
                 value={formData.eventType}
               >
@@ -169,15 +173,15 @@ const StepOne: FC<StepOneProps> = ({
               </Select>
             </div>
           </div>
-          <div className=''>
-            <p className='mb-2'>Event Category </p>
-            <div className='mb-5'>
+          <div className="">
+            <p className="mb-2">Event Category </p>
+            <div className="mb-5">
               <Select
-                label='Coporate'
-                className='bg-[#eeebfc]'
-                name='eventCategory'
+                label="Coporate"
+                className="!bg-input_background"
+                name="eventCategory"
                 onChange={(e: any) => {
-                  handleSelect('eventCategory', e);
+                  handleSelect("eventCategory", e);
                 }}
                 value={formData.eventCategory}
               >
@@ -189,15 +193,15 @@ const StepOne: FC<StepOneProps> = ({
               </Select>
             </div>
           </div>
-          <div className=''>
-            <p className='mb-2'>Event Sub Category</p>
-            <div className=' mb-5'>
+          <div className="">
+            <p className="mb-2">Event Sub Category</p>
+            <div className=" mb-5">
               <Select
-                label='General Meeting'
-                className='bg-[#eeebfc]'
-                name='eventSubCategory'
+                label="General Meeting"
+                className="!bg-input_background"
+                name="eventSubCategory"
                 onChange={(e: any) => {
-                  handleSelect('eventSubCategory', e);
+                  handleSelect("eventSubCategory", e);
                 }}
                 value={formData.eventSubCategory}
               >
@@ -209,15 +213,15 @@ const StepOne: FC<StepOneProps> = ({
               </Select>
             </div>
           </div>
-          <div className=''>
-            <p className='mb-2'>Event Budget</p>
-            <div className=' mb-8'>
+          <div className="">
+            <p className="mb-2">Event Budget</p>
+            <div className=" mb-8">
               <Select
-                label='$70,000 - $150,000'
-                className='bg-[#eeebfc]'
-                name='eventBudget'
+                label="$70,000 - $150,000"
+                className="!bg-input_background"
+                name="eventBudget"
                 onChange={(e: any) => {
-                  handleSelect('eventBudget', e);
+                  handleSelect("eventBudget", e);
                 }}
                 value={formData.eventBudget}
               >
@@ -230,66 +234,80 @@ const StepOne: FC<StepOneProps> = ({
             </div>
           </div>
         </div>
-        <p className='font-medium text-[18px] mb-4'>Address & Location</p>
-        <div className='grid grid-cols-2 gap-x-16 gap-y-4 mb-4 font-medium text-[16px] text-[#353535]'>
-          <div className=''>
-            <p className='mb-2'>Venue Name</p>
-            <div className='mb-5'>
+
+        <p className="font-medium text-[18px] mb-4">Address & Location</p>
+        <div className="grid grid-cols-2 gap-x-16 gap-y-4 mb-4 font-medium text-[16px] text-[#353535]">
+          <div className="">
+            <p className="mb-2">Venue Name</p>
+            <div className="mb-5 bg-input_background rounded-full">
               <Input
-                label='Venue Name'
-                crossOrigin=''
-                className='rounded-md bg-[#eeebfb]'
-                {...register('address.venueName')}
-                onChange={(e: any) => {
-                  handleSelect('address.venueName', e.target.value);
+                className="rounded-full !border !border-gray-300 text-gray-900 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-full"
+                labelProps={{
+                  className: "hidden",
                 }}
-                value={formData.address?.venueName || ''}
+                containerProps={{ className: "min-w-[100px]" }}
+                placeholder="Venue Name"
+                crossOrigin=""
+                {...register("address.venueName")}
+                onChange={(e: any) => {
+                  handleSelect("address.venueName", e.target.value);
+                }}
+                value={formData.address?.venueName || ""}
               />
             </div>
           </div>
 
-          <div className='...'>
-            <p className='mb-2'>Address</p>
-            <div className='mb-5'>
+          <div className="...">
+            <p className="mb-2">Address</p>
+            <div className="mb-5 bg-input_background rounded-full">
               <Input
-                label='Address'
-                crossOrigin=''
-                className='rounded-md bg-[#eeebfb]'
-                {...register('address.venueAddress')}
+                className="rounded-full !border !border-gray-300 text-gray-900 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-full"
+                labelProps={{
+                  className: "hidden",
+                }}
+                containerProps={{ className: "min-w-[100px]" }}
+                placeholder="Address"
+                crossOrigin=""
+                {...register("address.venueAddress")}
                 onChange={(e: any) => {
-                  handleSelect('address.venueAddress', e.target.value);
+                  handleSelect("address.venueAddress", e.target.value);
                 }}
                 value={formData.address?.venueAddress}
               />
             </div>
           </div>
         </div>
-        <div className='grid grid-cols-3 gap-x-16 gap-y-4 font-medium text-[16px] text-[#353535]'>
-          <div className='...'>
-            <p className='mb-2'>City </p>
-            <div className='mb-5'>
+
+        <div className="grid grid-cols-3 gap-x-16 gap-y-4 font-medium text-[16px] text-[#353535]">
+          <div className="...">
+            <p className="mb-2">City </p>
+            <div className="mb-5 bg-input_background rounded-full">
               <Input
-                label='Enter City'
-                crossOrigin=''
-                className='rounded-md bg-[#eeebfb]'
-                {...register('address.city')}
+                className="rounded-full !border !border-gray-300 text-gray-900 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-full"
+                labelProps={{
+                  className: "hidden",
+                }}
+                containerProps={{ className: "min-w-[100px]" }}
+                placeholder="Enter City"
+                crossOrigin=""
+                {...register("address.city")}
                 onChange={(e: any) => {
-                  handleSelect('address.city', e.target.value);
+                  handleSelect("address.city", e.target.value);
                 }}
                 value={formData.address?.city}
               />
             </div>
           </div>
 
-          <div className='...'>
-            <p className='mb-2'>State</p>
-            <div className='mb-5'>
+          <div className="...">
+            <p className="mb-2">State</p>
+            <div className="mb-5">
               <Select
-                label='Select State'
-                className='bg-[#eeebfc]'
-                name='address.state'
+                label="Select State"
+                className="!bg-input_background"
+                name="address.state"
                 onChange={(e: any) => {
-                  handleSelect('address.state', e);
+                  handleSelect("address.state", e);
                 }}
                 value={formData.address?.state}
               >
@@ -302,48 +320,52 @@ const StepOne: FC<StepOneProps> = ({
             </div>
           </div>
 
-          <div className='...'>
-            <p className='mb-2'>Zip</p>
-            <div className='mb-8'>
+          <div className="...">
+            <p className="mb-2">Zip</p>
+            <div className="mb-8 bg-input_background rounded-full">
               <Input
-                label='Enter Zip Code'
-                crossOrigin=''
-                className='rounded-md bg-[#eeebfb]'
-                {...register('address.zipCode')}
+                className="rounded-full !border !border-gray-300 text-gray-900 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10 rounded-full"
+                labelProps={{
+                  className: "hidden",
+                }}
+                containerProps={{ className: "min-w-[100px]" }}
+                placeholder="Enter Zip Code"
+                crossOrigin=""
+                {...register("address.zipCode")}
                 onChange={(e: any) => {
-                  handleSelect('address.zipCode', e.target.value);
+                  handleSelect("address.zipCode", e.target.value);
                 }}
                 value={formData.address?.zipCode}
               />
             </div>
           </div>
         </div>
-        <p className='font-medium text-[18px] mb-4'>File Attachment</p>
-        
+        <p className="font-medium text-[18px] mb-4">File Attachment</p>
+
         {uploadedFiles.length > 0 && (
-          <div className='grid grid-cols-2 gap-x-16 gap-y-4 font-medium text-[18px] text-black mb-2'>
+          <div className="grid grid-cols-2 gap-x-16 gap-y-4 font-medium text-[18px] text-black mb-2">
             {uploadedFiles.map((file, index) => (
-              <div key={index} className='mb-4'>
-                <div className='flex items-center gap-4'>
+              <div key={index} className="mb-4">
+                <div className="flex items-center gap-4">
                   <p>{file.name}</p>
                   <img
                     src={DELETE_BUTTON}
-                    alt='delete'
-                    className='object-scale-down w-[34px]'
+                    alt="delete"
+                    className="object-scale-down w-[34px]"
                   />
                 </div>
-                <div className='relative pt-1'>
-                  <div className='flex mb-2 items-center justify-between'>
+                <div className="relative pt-1">
+                  <div className="flex mb-2 items-center justify-between">
                     <div>
-                      <span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-teal-600 bg-teal-200'>
+                      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-teal-600 bg-teal-200">
                         {file.progress}%
                       </span>
                     </div>
                   </div>
-                  <div className='flex h-2 mb-4 overflow-hidden bg-gray-200 rounded'>
+                  <div className="flex h-2 mb-4 overflow-hidden bg-gray-200 rounded">
                     <div
                       style={{ width: `${file.progress}%` }}
-                      className='shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500'
+                      className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-teal-500"
                     ></div>
                   </div>
                 </div>
@@ -352,15 +374,15 @@ const StepOne: FC<StepOneProps> = ({
           </div>
         )}
         {formData.files && formData.files.length > 0 ? (
-          <div className='grid grid-cols-2 gap-x-16 gap-y-4 font-medium text-[18px] text-black mb-2'>
+          <div className="grid grid-cols-2 gap-x-16 gap-y-4 font-medium text-[18px] text-black mb-2">
             {formData.files.map((file: any, index: number) => (
-              <div key={index} className='mb-4'>
-                <div className='flex items-center gap-4'>
+              <div key={index} className="mb-4">
+                <div className="flex items-center gap-4">
                   <p>{file.fileName}</p>
                   <img
                     src={DELETE_BUTTON}
-                    alt='delete'
-                    className='object-scale-down w-[34px]'
+                    alt="delete"
+                    className="object-scale-down w-[34px]"
                   />
                 </div>
                 {/* (Progress bar code here if needed) */}
@@ -371,18 +393,18 @@ const StepOne: FC<StepOneProps> = ({
           <p>No files uploaded</p>
         )}
         <Button
-          variant='filled'
-          color='indigo'
-          size='sm'
-          className='rounded-md py-2 mt-4 px-4 bg-primary font-poppins'
+          variant="filled"
+          color="indigo"
+          size="sm"
+          className="rounded-md py-2 mt-4 px-4 bg-primary font-poppins"
         >
-          <label className='text-white normal-case font-medium'>
+          <label className="text-white normal-case font-medium">
             Upload File
             <input
-              type='file'
-              accept='.pdf, .doc, .docx, .jpg, .jpeg, .png'
+              type="file"
+              accept=".pdf, .doc, .docx, .jpg, .jpeg, .png"
               onChange={handleFileUpload}
-              className='hidden' // This hides the file input element
+              className="hidden" // This hides the file input element
               multiple
             />
           </label>
