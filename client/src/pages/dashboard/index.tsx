665c981e6d457595be2f6370bd3edf5be743f306
diduladishan <diduladishan14@gmail.com>
2023-11-29 04:34:34 +0530

    UI Updated

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,17 +1,23 @@
-import { useGetCurrentUser } from '../../app/hooks/useUser';
-import EditProfile from './components/edit-profile';
-import MemberTable from './components/member-table';
-import ResetPassword from './components/reset-password';
-import UploadInsurance from './components/upload-insurance';
+import { useGetCurrentUser } from "../../app/hooks/useUser";
+import EditProfile from "./components/edit-profile";
+import MemberTable from "./components/member-table";
+import ResetPassword from "./components/reset-password";
+import UploadInsurance from "./components/upload-insurance";
 
 function Index() {
   const user = useGetCurrentUser();
 
   return (
-    <div className='container mx-auto'>
-      <EditProfile user={user} />
-      <MemberTable user={user} />
-      {user?.userType === 'PROVIDER' && <UploadInsurance />}
+    <div className="container mx-auto">
+      <div className="z-1">
+        <EditProfile user={user} />
+      </div>
+
+      <div>
+        <MemberTable user={user} />
+      </div>
+
+      {user?.userType === "PROVIDER" && <UploadInsurance />}
       <ResetPassword />
     </div>
   );
