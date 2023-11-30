78429475ebe9eca40b138860f574bc9db8499a0d
diduladishan <diduladishan14@gmail.com>
2023-11-30 18:04:46 +0530

    10.2_Event_detail_page drag and drop added

--- client/src/pages/events/event-details/components/SubmitProposal.tsx
+++ client/src/pages/events/event-details/components/SubmitProposal.tsx
@@ -9,18 +9,23 @@ export function SubmitProposal() {
   const handleOpen = () => setOpen((cur) => !cur);
 
   return (
-    <div className="bg-[#F8F8FF] border-dashed border-2 border-[#6f7070] m-6 p-4 h-[400px] flex items-center cursor-pointer">
-      <div>
-        <div className="flex items-center justify-center mb-4">
-          <img
-            src={UPLOAD_ICON}
-            alt="aad"
-            className="object-scale-down w-[50px]"
-          />
-        </div>
+    <div>
+      <div className="bg-[#F8F8FF] border-dashed border-2 border-[#6f7070] m-6 p-4 h-[400px] flex items-center cursor-pointer">
+        <div>
+          <div className="flex items-center justify-center ">
+            <img
+              src={UPLOAD_ICON}
+              alt="aad"
+              className="object-scale-down w-[50px]"
+            />
+          </div>
 
-        <Dragdrop />
+          <Dragdrop />
+        </div>
       </div>
+
+      <div></div>
+      <Button className="mx-6 mb-6 w-[485px] bg-primary">Upload Files</Button>
     </div>
   );
 }
