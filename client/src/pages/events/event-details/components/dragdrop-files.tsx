1fc8e1a22d37b8bc7253b1e1d7baaefc87deb396
diduladishan <diduladishan14@gmail.com>
2023-12-01 20:17:15 +0530

    drah and drop (file name and size added)

--- client/src/pages/events/event-details/components/dragdrop-files.tsx
+++ client/src/pages/events/event-details/components/dragdrop-files.tsx
@@ -1,7 +1,12 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 function MyDropzone() {
+  const [fileInfo, setFileInfo] = useState<{
+    name: string;
+    size: number;
+  } | null>(null);
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
     acceptedFiles.forEach((file: File) => {
       const reader = new FileReader();
@@ -14,20 +19,37 @@ function MyDropzone() {
         console.log(binaryStr);
       };
       reader.readAsArrayBuffer(file);
+
+      // Set file info to state
+      setFileInfo({
+        name: file.name,
+        size: file.size,
+      });
     });
   }, []);
+
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <p className="font-semibold text-center mb-2">
-        Drag & drop files or
-        <span className="font-normal underline ml-2">Browse</span>
-      </p>
-      <div className="text-[#676767] text-center">
-        Supported formates: JPEG, PNG, GIF, MP4, PDF, PSD, AI, Word, PPT
+    <div>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p className="font-semibold text-center mb-2">
+          Drag & drop files or
+          <span className="font-normal underline ml-2">Browse</span>
+        </p>
+        <div className="text-[#676767] text-center">
+          Supported formats: JPEG, PNG, GIF, MP4, PDF, PSD, AI, Word, PPT
+        </div>
       </div>
+
+      {/* Display file info on the webpage */}
+      {fileInfo && (
+        <div className="text-center mt-4">
+          <p>File Name: {fileInfo.name}</p>
+          <p>File Size: {fileInfo.size} bytes</p>
+        </div>
+      )}
     </div>
   );
 }
