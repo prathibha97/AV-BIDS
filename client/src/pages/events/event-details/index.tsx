05feaf50b52948a713a9b953455da986f6c03bc2
diduladishan <diduladishan14@gmail.com>
2023-12-08 07:34:43 +0530

    event details page updated

--- client/src/pages/events/event-details/index.tsx
+++ client/src/pages/events/event-details/index.tsx
@@ -1,25 +1,28 @@
-import { MdOutlineCancel } from 'react-icons/md';
-import EVENTDETAILS_03 from '../../../assets/13_event_details_page/Rectangle 3759.png';
-import EVENTDETAILS_02 from '../../../assets/13_event_details_page/carbon_time.png';
-import SPAM_ICON from '../../../assets/13_event_details_page/spam.png';
+import { MdOutlineCancel } from "react-icons/md";
+import EVENTDETAILS_03 from "../../../assets/13_event_details_page/Rectangle 3759.png";
+import EVENTDETAILS_02 from "../../../assets/13_event_details_page/carbon_time.png";
+import SPAM_ICON from "../../../assets/13_event_details_page/spam.png";
 
-import { Button, Dialog, Textarea } from '@material-tailwind/react';
-import { format } from 'date-fns';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Spinner from '../../../components/spinner';
-import { Event, UserWithReviewWithEvent } from '../../../types';
-import api from '../../../utils/api';
-import SubmitProposal from './components/SubmitProposal';
-import Attachments from './components/attachments';
-import EventInfo from './components/event-info';
-import EventPlanner from './components/event-planner';
-import OtherEvents from './components/other-events';
-import { useGetCurrentUser } from '../../../app/hooks/useUser';
-import AlertBox from '../../../components/alert-box';
-import { useAppDispatch, useAppSelector } from '../../../app/hooks';
-import { RootState } from '../../../app/store';
-import { setAlert, setAlertWithTimeout } from '../../../app/features/alerts/alertSlice';
+import { Button, Dialog, Textarea, Input } from "@material-tailwind/react";
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../../../components/spinner";
+import { Event, UserWithReviewWithEvent } from "../../../types";
+import api from "../../../utils/api";
+import SubmitProposal from "./components/SubmitProposal";
+import Attachments from "./components/attachments";
+import EventInfo from "./components/event-info";
+import EventPlanner from "./components/event-planner";
+import OtherEvents from "./components/other-events";
+import { useGetCurrentUser } from "../../../app/hooks/useUser";
+import AlertBox from "../../../components/alert-box";
+import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+import { RootState } from "../../../app/store";
+import {
+  setAlert,
+  setAlertWithTimeout,
+} from "../../../app/features/alerts/alertSlice";
 
 export function Index() {
   const { id } = useParams();
@@ -36,7 +39,7 @@ export function Index() {
     (state: RootState) => state.alert
   );
 
-  const user = useGetCurrentUser()
+  const user = useGetCurrentUser();
 
   const handleOpen = () => setOpenProposalDialog((cur) => !cur);
 
@@ -46,7 +49,7 @@ export function Index() {
       const { data } = await api.get(`/events/${id}`);
       setEvent(data);
     } catch (error) {
-      console.error('Error fetching event details:', error);
+      console.error("Error fetching event details:", error);
     } finally {
       setEventLoading(false);
     }
@@ -60,7 +63,7 @@ export function Index() {
         setUserEvents(data);
       }
     } catch (error) {
-      console.error('Error fetching user events:', error);
+      console.error("Error fetching user events:", error);
     } finally {
       setUserEventLoading(false);
     }
@@ -74,7 +77,7 @@ export function Index() {
         setPlanner(data);
       }
     } catch (error) {
-      console.error('Error fetching planner info:', error);
+      console.error("Error fetching planner info:", error);
     } finally {
       setPlannerLoading(false);
     }
@@ -82,11 +85,11 @@ export function Index() {
 
   const handleSaveEvent = async () => {
     try {
-      const {data} = await api.post(`/events/save/${event?._id}`);
+      const { data } = await api.post(`/events/save/${event?._id}`);
       dispatch(
         setAlertWithTimeout({
           message: data.message,
-          color: 'green',
+          color: "green",
           open: true,
         })
       );
@@ -95,15 +98,15 @@ export function Index() {
         dispatch(
           setAlertWithTimeout({
             message: error.response.data.error,
-            color: 'red',
+            color: "red",
             open: true,
           })
         );
       } else if (error.request) {
-        console.log('No response received from the server.');
+        console.log("No response received from the server.");
       } else {
         // Something happened in setting up the request that triggered an Error
-        console.log('Error while setting up the request:', error.message);
+        console.log("Error while setting up the request:", error.message);
       }
     }
   };
@@ -127,171 +130,227 @@ export function Index() {
   const status =
     eventStartDate && eventEndDate
       ? currentDate <= eventStartDate && currentDate <= eventEndDate
-        ? 'Active'
-        : 'Expired'
-      : 'N/A';
+        ? "Active"
+        : "Expired"
+      : "N/A";
 
   if (eventLoading) {
     return (
-      <div className='flex items-center justify-center h-full'>
+      <div className="flex items-center justify-center h-full">
         <Spinner />
       </div>
     );
   }
 
   return (
-    <div className='mx-auto mt-16'>
+    <div className="mx-auto mt-16">
       <AlertBox
         color={color}
-        variant='ghost'
+        variant="ghost"
         text={message!}
         open={open}
         setOpen={() =>
-          dispatch(setAlert({ open: false, message: '', color: 'green' }))
+          dispatch(setAlert({ open: false, message: "", color: "green" }))
         }
       />
-      <div className='grid lg:grid-cols-3 gap-4 content-center'>
-        <div className='col-span-2 flex justify-center items-center px-8'>
+      <div className="grid lg:grid-cols-3 gap-4 content-center">
+        <div className="col-span-2 flex justify-center items-center px-8">
           <section>
             <div>
               <div>
-                <h2 className='text-primary text-[40px] mb-4'>
+                <h2 className="text-primary text-[40px] mb-4">
                   {event?.title}
                 </h2>
               </div>
-              <div className='flex items-center mb-4 gap-8'>
-                <div className='flex items-center gap-2'>
+              <div className="flex items-center mb-4 gap-8">
+                <div className="flex items-center gap-2">
                   <img
                     src={EVENTDETAILS_02}
-                    alt='aad'
-                    className='object-scale-down w-[22px]'
+                    alt="aad"
+                    className="object-scale-down w-[22px]"
                   />
                   <p>
-                    Posted on{' '}
+                    Posted on{" "}
                     {event?.createdAt
-                      ? format(new Date(event.createdAt), 'M/dd/yyyy')
-                      : 'N/A'}
+                      ? format(new Date(event.createdAt), "M/dd/yyyy")
+                      : "N/A"}
                   </p>
                 </div>
 
-                <div className='flex items-center gap-2'>
+                <div className="flex items-center gap-2">
                   <img
                     src={EVENTDETAILS_02}
-                    alt='aad'
-                    className='object-scale-down w-[22px]'
+                    alt="aad"
+                    className="object-scale-down w-[22px]"
                   />
                   <p>
-                    Updated on{' '}
+                    Updated on{" "}
                     {event?.createdAt
-                      ? format(new Date(event.updatedAt), 'M/dd/yyyy')
-                      : 'N/A'}
+                      ? format(new Date(event.updatedAt), "M/dd/yyyy")
+                      : "N/A"}
                   </p>
                 </div>
 
-                <div className='flex items-center gap-2'>
-                  <p className='font-semibold'>Status:</p>
+                <div className="flex items-center gap-2">
+                  <p className="font-semibold">Status:</p>
                   <div
                     className={`bg-${
-                      status === 'Expired' ? 'red' : 'green'
+                      status === "Expired" ? "red" : "green"
                     }-500 rounded-full px-4 py-0.5`}
                   >
-                    <p className='text-white text-center'>{status}</p>
+                    <p className="text-white text-center">{status}</p>
                   </div>
                 </div>
               </div>
-              <div className='mb-6'>
+              <div className="mb-6">
                 <img
                   src={EVENTDETAILS_03}
-                  alt='aad'
-                  className='object-scale-down'
+                  alt="aad"
+                  className="object-scale-down"
                 />
               </div>
-              <div className='bg-[#F3F1FB] p-6 mb-16 rounded-lg'>
-                <h2 className='text-[20px] mb-4'>Description</h2>
+              <div className="bg-[#F3F1FB] p-6 mb-16 rounded-lg">
+                <h2 className="text-[20px] mb-4">Description</h2>
                 <div
                   dangerouslySetInnerHTML={{ __html: event?.description! }}
                 />
               </div>
+
+              <OtherEvents events={userEvents} loading={userEventLoading} />
+
               <div>
-                <h2 className='text-[20px] mb-4'>
+                <h2 className="text-[20px] mb-4">
                   Submit a question about the event
                 </h2>
-                <p className='text-[20px] mb-2'>Description</p>
-                <div className='rounded-lg'>
-                  <div className='mb-6'>
-                    <Textarea
-                      label='Description'
-                      className='!bg-[#F3F1FB] border-solid border-2 border-[#E4E4E4]'
-                    />
+
+                <div className="grid grid-cols-2 gap-y-8 gap-x-6">
+                  <div>
+                    <p className="text-[16px] mb-2 font-medium">
+                      Email Address
+                    </p>
+                    <div className="w-full bg-input_background rounded-full">
+                      <Input
+                        className="rounded-full !border !border-gray-300 text-gray-900 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+                        labelProps={{
+                          className: "hidden",
+                        }}
+                        containerProps={{ className: "min-w-[100px]" }}
+                        crossOrigin=""
+                        placeholder="example@example.com"
+                      />
+                    </div>
                   </div>
 
-                  <div className='flex justify-end mb-16'>
-                    <Button
-                      variant='filled'
-                      color='indigo'
-                      size='sm'
-                      className='rounded-full w-30 py-3 px-6 mt-4  bg-primary font-poppins'
-                    >
-                      <span className='text-white normal-case text-[14px]'>
-                        Submit
-                      </span>
-                    </Button>
+                  <div>
+                    <p className="text-[16px] mb-2 font-medium">Phone Number</p>
+                    <div className="w-full bg-input_background rounded-full">
+                      <Input
+                        className="rounded-full !border !border-gray-300 text-gray-900 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+                        labelProps={{
+                          className: "hidden",
+                        }}
+                        containerProps={{ className: "min-w-[100px]" }}
+                        crossOrigin=""
+                        placeholder="Phone Number"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-span-2">
+                    <p className="text-[16px] mb-2 font-medium">Subject</p>
+                    <div className="w-full bg-input_background rounded-full">
+                      <Input
+                        className="rounded-full !border !border-gray-300 text-gray-900 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+                        labelProps={{
+                          className: "hidden",
+                        }}
+                        containerProps={{ className: "min-w-[100px]" }}
+                        crossOrigin=""
+                        placeholder="Phone Number"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="rounded-lg col-span-2">
+                    <p className="text-[16px] mb-2 font-medium">Description</p>
+                    <div className="mb-6">
+                      <Textarea
+                        label="Description"
+                        className="!bg-[#F3F1FB] border-solid border-2 border-[#E4E4E4]"
+                      />
+                    </div>
+
+                    <div className="flex justify-end mb-16">
+                      <Button
+                        variant="filled"
+                        color="indigo"
+                        size="sm"
+                        className="rounded-full w-30 py-3 px-6 mt-4  bg-primary font-poppins"
+                      >
+                        <span className="text-white normal-case text-[14px]">
+                          Submit
+                        </span>
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </div>
-              <OtherEvents events={userEvents} loading={userEventLoading} />
             </div>
           </section>
           <section>
-            <Dialog open={openProposalDialog} handler={handleOpen} size='xs'>
-              <div className='flex justify-end p-3'>
+            <Dialog open={openProposalDialog} handler={handleOpen} size="xs">
+              <div className="flex justify-end p-3">
                 <MdOutlineCancel
                   size={32}
-                  className='text-black cursor-pointer'
+                  className="text-black cursor-pointer"
                   onClick={handleOpen}
                 />
               </div>
-              <SubmitProposal handleOpen={handleOpen} event={event} user={user}/>
+              <SubmitProposal
+                handleOpen={handleOpen}
+                event={event}
+                user={user}
+              />
             </Dialog>
           </section>
         </div>
-        <div className='flex items-start'>
+        <div className="flex items-start">
           <section>
             {/* Only provider can submit proposals and save events */}
-            {user?.userType === 'PROVIDER' && (
+            {user?.userType === "PROVIDER" && (
               <>
-                <div className='mb-4'>
+                <div className="mb-4">
                   <Button
-                    variant='filled'
-                    color='indigo'
-                    size='sm'
-                    className='rounded-full w-full py-4 mt-4 px-8 bg-primary font-poppins'
+                    variant="filled"
+                    color="indigo"
+                    size="sm"
+                    className="rounded-full w-full py-4 mt-4 px-8 bg-primary font-poppins"
                     onClick={handleOpen}
                   >
-                    <span className='text-white normal-case text-[14px]'>
+                    <span className="text-white normal-case text-[14px]">
                       Submit Proposal
                     </span>
                   </Button>
 
                   <Button
-                    variant='outlined'
-                    size='sm'
-                    className='rounded-full w-full py-4 mt-4 px-8 font-poppins'
+                    variant="outlined"
+                    size="sm"
+                    className="rounded-full w-full py-4 mt-4 px-8 font-poppins"
                     onClick={() => handleSaveEvent()}
                   >
-                    <span className=' text-black normal-case text-[14px]'>
+                    <span className=" text-black normal-case text-[14px]">
                       Save Event
                     </span>
                   </Button>
                 </div>
 
-                <div className='flex items-center justify-center gap-3 mb-6 '>
+                <div className="flex items-center justify-center gap-3 mb-6 ">
                   <img
                     src={SPAM_ICON}
-                    alt='aad'
-                    className='object-scale-down w-[24px]'
+                    alt="aad"
+                    className="object-scale-down w-[24px]"
                   />
-                  <p className='text-[18px] underline'>Flag as spam</p>
+                  <p className="text-[18px] underline">Flag as spam</p>
                 </div>
               </>
             )}
