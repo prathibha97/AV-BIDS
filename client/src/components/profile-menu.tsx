f79e10548e569ab673afff77e8c7efdbd572a0b4
diduladishan <diduladishan14@gmail.com>
2023-12-09 04:23:34 +0530

    notifications and functions completed

--- client/src/components/profile-menu.tsx
+++ client/src/components/profile-menu.tsx
@@ -1,4 +1,4 @@
-import { InboxArrowDownIcon, PowerIcon } from '@heroicons/react/24/solid';
+import { InboxArrowDownIcon, PowerIcon } from "@heroicons/react/24/solid";
 import {
   Avatar,
   Badge,
@@ -7,15 +7,17 @@ import {
   MenuItem,
   MenuList,
   Typography,
-} from '@material-tailwind/react';
-import React from 'react';
-import { MdNotificationsNone } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
-import { clearUser } from '../app/features/user/userSlice';
-import { useAppDispatch } from '../app/hooks';
+} from "@material-tailwind/react";
+import React from "react";
+import { MdNotificationsNone } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import { clearUser } from "../app/features/user/userSlice";
+import { useAppDispatch } from "../app/hooks";
 
-import { useGetCurrentUser } from '../app/hooks/useUser';
-import api from '../utils/api';
+import { useGetCurrentUser } from "../app/hooks/useUser";
+import api from "../utils/api";
+
+import Notification_bell from "./notification-bell";
 
 function ProfileMenu() {
   const navigate = useNavigate();
@@ -28,10 +30,10 @@ function ProfileMenu() {
 
   const handleSignout = async () => {
     try {
-      await api.post('/auth/logout');
-      localStorage.removeItem('userInfo');
+      await api.post("/auth/logout");
+      localStorage.removeItem("userInfo");
       dispatch(clearUser());
-      navigate('/');
+      navigate("/");
     } catch (error) {
       console.error(error);
     }
@@ -39,17 +41,17 @@ function ProfileMenu() {
 
   const profileMenuItems = [
     {
-      label: 'Dashboard',
+      label: "Dashboard",
       icon: InboxArrowDownIcon,
-      onClick: () => navigate('/dashboard'),
+      onClick: () => navigate("/dashboard"),
     },
     {
-      label: 'Inbox',
+      label: "Inbox",
       icon: InboxArrowDownIcon,
-      onClick: () => navigate('/messages'),
+      onClick: () => navigate("/messages"),
     },
     {
-      label: 'Sign Out',
+      label: "Sign Out",
       icon: PowerIcon,
       onClick: () => {
         handleSignout();
@@ -59,33 +61,33 @@ function ProfileMenu() {
   ];
 
   return (
-    <div className='flex items-center gap-6 cursor-pointer'>
-      <Badge content='5' color='green'>
-        <MdNotificationsNone className='text-[#000000] text-[30px]' />
-      </Badge>
-
-      <Menu open={isMenuOpen} handler={setIsMenuOpen} placement='bottom-end'>
+    <div className="flex items-center gap-6 cursor-pointer">
+      {/* <Badge content="5" color="green">
+        <MdNotificationsNone className="text-[#000000] text-[30px]" />
+      </Badge> */}
+      <Notification_bell />
+      <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
         <MenuHandler>
-          <div className='flex gap-2'>
+          <div className="flex gap-2">
             <Avatar
-              variant='circular'
-              size='sm'
-              alt='tania andrew'
-              className='border border-gray-900 p-0.5'
+              variant="circular"
+              size="sm"
+              alt="tania andrew"
+              className="border border-gray-900 p-0.5"
               src={
                 user?.imageUrl
                   ? `https://av-bids-bucket.s3.ap-south-1.amazonaws.com/${user?.imageUrl}`
-                  : 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+                  : "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
               }
             />
           </div>
         </MenuHandler>
-        <MenuList className='p-1'>
+        <MenuList className="p-1">
           {profileMenuItems.map(({ label, icon, onClick }, key) => {
             const isLastItem = key === profileMenuItems.length - 1;
             const dynamicStyles = isLastItem
-              ? 'hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10'
-              : '';
+              ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+              : "";
 
             return (
               <MenuItem
@@ -94,14 +96,14 @@ function ProfileMenu() {
                 className={`flex items-center gap-2 rounded ${dynamicStyles}`}
               >
                 {React.createElement(icon, {
-                  className: `h-4 w-4 ${isLastItem ? 'text-red-500' : ''}`,
+                  className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
                   strokeWidth: 2,
                 })}
                 <Typography
-                  as='span'
-                  variant='small'
-                  className='font-normal'
-                  color={isLastItem ? 'red' : 'inherit'}
+                  as="span"
+                  variant="small"
+                  className="font-normal"
+                  color={isLastItem ? "red" : "inherit"}
                 >
                   {label}
                 </Typography>
--- client/src/layout/NavbarDashboard.tsx
+++ client/src/layout/NavbarDashboard.tsx
@@ -1,11 +1,11 @@
-import { Bars2Icon } from '@heroicons/react/24/solid';
-import { Button, IconButton, Navbar } from '@material-tailwind/react';
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Bars2Icon } from "@heroicons/react/24/solid";
+import { Button, IconButton, Navbar } from "@material-tailwind/react";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import { useGetCurrentUser } from '../app/hooks/useUser';
-import PLUS_ICON from '../assets/navigation bar/plus.png';
-import ProfileMenu from '../components/profile-menu';
+import { useGetCurrentUser } from "../app/hooks/useUser";
+import PLUS_ICON from "../assets/navigation bar/plus.png";
+import ProfileMenu from "../components/profile-menu";
 
 // nav list component
 export function NavbarDashboard() {
@@ -18,78 +18,78 @@ export function NavbarDashboard() {
 
   React.useEffect(() => {
     window.addEventListener(
-      'resize',
+      "resize",
       () => window.innerWidth >= 960 && setIsNavOpen(false)
     );
   }, []);
 
   const navList = (
-    <ul className='mb-4 mt-2 flex justify-center flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
-      <Link to='/events'>
-        <p className='text-gray-600 font-medium'>Events Page</p>
+    <ul className="mb-4 mt-2 flex justify-center flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Link to="/events">
+        <p className="text-gray-600 font-medium">Events Page</p>
       </Link>
     </ul>
   );
 
- return (
-   <div>
-     <div className='w-full'>
-       <Navbar className='mx-auto max-w-screen-xl p-2 lg:pl-6 bg-[#f7f6fd] shadow-none border-none py-6'>
-         <div className='relative mx-auto flex items-center justify-between text-blue-gray-900'>
-           {user && user?.userType === 'PLANNER' && (
-             <Button
-               variant='outlined'
-               size='sm'
-               className='lg:inline-block rounded-btn '
-               onClick={() => navigate('/events/new')}
-             >
-               <div className='flex items-center gap-2'>
-                 <img src={PLUS_ICON} alt='aad' className='object-contain' />
-                 <span className='text-black normal-case'>Post New Event</span>
-               </div>
-             </Button>
-           )}
+  return (
+    <div>
+      <div className="w-full">
+        <Navbar className="mx-auto max-w-screen-xl p-2 lg:pl-6 bg-[#f7f6fd] shadow-none border-none py-6">
+          <div className="relative mx-auto flex items-center justify-between text-blue-gray-900">
+            {user && user?.userType === "PLANNER" && (
+              <Button
+                variant="outlined"
+                size="sm"
+                className="lg:inline-block rounded-btn "
+                onClick={() => navigate("/events/new")}
+              >
+                <div className="flex items-center gap-2">
+                  <img src={PLUS_ICON} alt="aad" className="object-contain" />
+                  <span className="text-black normal-case">Post New Event</span>
+                </div>
+              </Button>
+            )}
 
-           <div className='lg:block flex-grow text-center'>{navList}</div>
+            <div className="lg:block flex-grow text-center">{navList}</div>
 
-           {user ? (
-             <>
-               <IconButton
-                 size='sm'
-                 color='blue-gray'
-                 variant='text'
-                 onClick={toggleIsNavOpen}
-                 className='ml-auto mr-2 lg:hidden'
-               >
-                 <Bars2Icon className='h-6 w-6' />
-               </IconButton>
-               <ProfileMenu />
-             </>
-           ) : (
-             <div className='lg:flex lg:items-center lg:justify-end'>
-               <Button
-                 variant='text'
-                 size='sm'
-                 className='lg:inline-block text-btn'
-               >
-                 <Link to='/sign-in'>
-                   <span className='text-black'>Login</span>
-                 </Link>
-               </Button>
-               <Button
-                 variant='outlined'
-                 size='sm'
-                 className='lg:inline-block rounded-btn ml-2'
-               >
-                 <Link to='/sign-up'>
-                   <span className='text-black'>Get Started</span>
-                 </Link>
-               </Button>
-             </div>
-           )}
-         </div>
-       </Navbar>
-     </div>
-   </div>
- );
+            {user ? (
+              <>
+                <IconButton
+                  size="sm"
+                  color="blue-gray"
+                  variant="text"
+                  onClick={toggleIsNavOpen}
+                  className="ml-auto mr-2 lg:hidden"
+                >
+                  <Bars2Icon className="h-6 w-6" />
+                </IconButton>
+                <ProfileMenu />
+              </>
+            ) : (
+              <div className="lg:flex lg:items-center lg:justify-end">
+                <Button
+                  variant="text"
+                  size="sm"
+                  className="lg:inline-block text-btn"
+                >
+                  <Link to="/sign-in">
+                    <span className="text-black">Login</span>
+                  </Link>
+                </Button>
+                <Button
+                  variant="outlined"
+                  size="sm"
+                  className="lg:inline-block rounded-btn ml-2"
+                >
+                  <Link to="/sign-up">
+                    <span className="text-black">Get Started</span>
+                  </Link>
+                </Button>
+              </div>
+            )}
+          </div>
+        </Navbar>
+      </div>
+    </div>
+  );
 }
