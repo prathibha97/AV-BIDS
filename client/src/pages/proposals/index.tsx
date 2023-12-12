6ba5b67ad95241cd1cba961c91140fa3a9c50982
diduladishan <diduladishan14@gmail.com>
2023-12-12 07:17:57 +0530

    UI responsiveness updated

--- client/src/pages/proposals/index.tsx
+++ client/src/pages/proposals/index.tsx
@@ -1,10 +1,10 @@
-import ProposalsTable from './components/table';
+import ProposalsTable from "./components/table";
 
 export function Index() {
   return (
-    <div className='container mx-auto mb-8'>
-      <section className='bg-white px-8 py-8 rounded-xl drop-shadow-md'>
-        <h2 className='text-[20px] font-semibold mb-6'>Proposals</h2>
+    <div className="container mx-auto mb-8">
+      <section className="bg-white px-8 py-8 rounded-xl drop-shadow-md min-w-max">
+        <h2 className="text-[20px] font-semibold mb-6">Proposals</h2>
         <ProposalsTable />
       </section>
     </div>
