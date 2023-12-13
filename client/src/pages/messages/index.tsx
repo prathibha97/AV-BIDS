4943d79344ce1b4675db0b02750b8609096cb8cf
Prathibha Ratnayake <prsthibha@gmail.com>
2023-12-13 09:18:06 +0530

    update sockert server cors error on prod branch

--- client/src/pages/messages/index.tsx
+++ client/src/pages/messages/index.tsx
@@ -32,7 +32,9 @@ function Index() {
   useEffect(() => {
     // @ts-ignore
     // socket.current = io('ws://localhost:5005');
-    socket.current = io('ws://54.201.46.218:5005');
+    // socket.current = io('ws://54.201.46.218:5005');
+    socket.current = io('ws://www.avbids.com:5005');
+
     // @ts-ignore
     socket.current.on('getMessage', (data) => {
       setArrivalMessage({
