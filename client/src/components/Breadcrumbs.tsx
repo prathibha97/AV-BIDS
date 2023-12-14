abf45e1b6e29cc5df777741cd7cfca3bcce31459
Prathibha Ratnayake <prsthibha@gmail.com>
2023-12-14 19:46:37 +0530

    UI improvement

--- client/src/components/Breadcrumbs.tsx
+++ client/src/components/Breadcrumbs.tsx
@@ -1,35 +1,42 @@
 // Breadcrumbs.tsx
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Breadcrumbs: React.FC = () => {
   const location = useLocation();
-  const pathnames = location.pathname.split("/").filter((x) => x);
+  const pathnames = location.pathname.split('/').filter((x) => x);
 
   return (
-    <nav className="mb-4 ml-4">
-      <ul className="flex items-center">
+    <nav className='mb-4 ml-4'>
+      <ul className='flex items-center'>
         <li>
-          <Link to="/" className="text-[#8645FF]">
-            Home
+          <Link to='/' className='text-[#8645FF]'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-4 w-4'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+            >
+              <path d='M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z' />
+            </svg>
           </Link>
         </li>
         {pathnames.map((name, index) => {
-          const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+          const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
           const isLast = index === pathnames.length - 1;
 
           return (
             <React.Fragment key={name}>
-              <li>
-                <span className="text-[12px] mx-2">➔</span>
+              <li className='flex items-center'>
+                <span className='text-[12px] mx-2'>/</span>
                 {!isLast ? (
                   <>
-                    <Link to={routeTo} className="text-[#8645FF]">
+                    <Link to={routeTo} className='text-[#8645FF]'>
                       {name}
                     </Link>
                   </>
                 ) : (
-                  <span className="text-black">{name}</span>
+                  <span className='text-black'>{name}</span>
                 )}
               </li>
             </React.Fragment>
