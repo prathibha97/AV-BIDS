3c75161ffe8eaa3f5efe074700b22e982b9687a5
diduladishan <diduladishan14@gmail.com>
2023-12-16 08:44:15 +0530

    frontend updated

--- client/src/pages/auth/sign-up/components/planner-account.tsx
+++ client/src/pages/auth/sign-up/components/planner-account.tsx
@@ -1,13 +1,13 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Checkbox, Input } from '@material-tailwind/react';
-import { FC, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import api from '../../../../utils/api';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Checkbox, Input } from "@material-tailwind/react";
+import { FC, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import api from "../../../../utils/api";
 import {
   RegisterFormSchema,
   RegisterFormValues,
-} from '../../../../utils/validations/register-form-validation';
+} from "../../../../utils/validations/register-form-validation";
 
 interface PlannerAccountFormProps {
   userType: string;
@@ -30,20 +30,20 @@ const PlannerAccountForm: FC<PlannerAccountFormProps> = ({
   } = useForm<RegisterFormValues>({
     resolver: zodResolver(RegisterFormSchema),
     defaultValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
     },
   });
 
   const onSubmit = async (values: RegisterFormValues) => {
     try {
-      await api.post('/auth/register', {
+      await api.post("/auth/register", {
         ...values,
         userType,
       });
-      navigate('/dashboard');
+      navigate("/dashboard");
     } catch (error: any) {
       if (error.response) {
         const errorMessage = error.response.data.message;
@@ -56,112 +56,119 @@ const PlannerAccountForm: FC<PlannerAccountFormProps> = ({
           setErrorMessage(null);
         }, 5000);
       } else if (error.request) {
-        console.log('No response received from the server.');
+        console.log("No response received from the server.");
       } else {
         // Something happened in setting up the request that triggered an Error
-        console.log('Error while setting up the request:', error.message);
+        console.log("Error while setting up the request:", error.message);
       }
     }
   };
   return (
     <>
       <form
-        className='mt-6 flex flex-col gap-4'
+        className="mt-6 flex flex-col gap-4"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div>
+        <div className="w-full !bg-input_background rounded-lg">
           <Input
-            type='text'
-            placeholder='First Name'
-            className=' !border-t-blue-gray-200 focus:!border-t-gray-900 bg-input_background rounded-full'
+            type="text"
+            placeholder="First Name"
+            className="!border !border-gray-300 !bg-input_background text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900"
             labelProps={{
-              className: 'before:content-none after:content-none',
+              className: "hidden",
             }}
-            crossOrigin='anonymous'
-            {...register('firstName')}
+            containerProps={{ className: "min-w-[100px]" }}
+            crossOrigin="anonymous"
+            {...register("firstName")}
           />
           {errors.firstName && (
-            <span className='text-red-500'>First Name is required</span>
-          )}
-        </div>
-        <div>
-          <Input
-            type='text'
-            placeholder='Last Name'
-            className=' !border-t-blue-gray-200 focus:!border-t-gray-900 bg-input_background rounded-full'
-            labelProps={{
-              className: 'before:content-none after:content-none',
-            }}
-            crossOrigin='anonymous'
-            {...register('lastName')}
-          />
-          {errors.lastName && (
-            <span className='text-red-500'>Last Name is required</span>
-          )}
-        </div>
-        <div>
-          <Input
-            type='email'
-            placeholder='Email Address'
-            className=' !border-t-blue-gray-200 focus:!border-t-gray-900 bg-input_background rounded-full'
-            labelProps={{
-              className: 'before:content-none after:content-none',
-            }}
-            crossOrigin='anonymous'
-            {...register('email')}
-          />
-          {errors.email && (
-            <span className='text-red-500'>Email is required</span>
-          )}
-        </div>
-        <div>
-          <Input
-            type='password'
-            placeholder='Password'
-            className=' !border-t-blue-gray-200 focus:!border-t-gray-900 bg-input_background rounded-full'
-            labelProps={{
-              className: 'before:content-none after:content-none',
-            }}
-            crossOrigin='anonymous'
-            {...register('password')}
-          />
-          {errors.password && (
-            <span className='text-red-500'>Password is required</span>
+            <span className="text-red-500">First Name is required</span>
           )}
         </div>
 
-        <div className='flex'>
-          <div className='self-start p-0 m-0'>
+        <div className="w-full !bg-input_background rounded-lg">
+          <Input
+            type="text"
+            placeholder="Last Name"
+            className="!border !border-gray-300 !bg-input_background text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900"
+            labelProps={{
+              className: "hidden",
+            }}
+            containerProps={{ className: "min-w-[100px]" }}
+            crossOrigin="anonymous"
+            {...register("lastName")}
+          />
+          {errors.lastName && (
+            <span className="text-red-500">Last Name is required</span>
+          )}
+        </div>
+
+        <div className="w-full !bg-input_background rounded-lg">
+          <Input
+            type="email"
+            placeholder="Email Address"
+            className="!border !border-gray-300 !bg-input_background text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900"
+            labelProps={{
+              className: "hidden",
+            }}
+            containerProps={{ className: "min-w-[100px]" }}
+            crossOrigin="anonymous"
+            {...register("email")}
+          />
+          {errors.email && (
+            <span className="text-red-500">Email is required</span>
+          )}
+        </div>
+
+        <div className="w-full !bg-input_background rounded-lg">
+          <Input
+            type="password"
+            placeholder="Password"
+            className="!border !border-gray-300 !bg-input_background text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900"
+            labelProps={{
+              className: "hidden",
+            }}
+            containerProps={{ className: "min-w-[100px]" }}
+            crossOrigin="anonymous"
+            {...register("password")}
+          />
+          {errors.password && (
+            <span className="text-red-500">Password is required</span>
+          )}
+        </div>
+
+        <div className="flex">
+          <div className="self-start p-0 m-0">
             <Checkbox
-              className='border-black items-start'
-              crossOrigin=''
+              className="border-black items-start"
+              crossOrigin=""
               onClick={() => setChecked((prev) => !prev)}
             />
           </div>
 
-          <p color='blue-gray' className='text-[14px] text-black'>
+          <p color="blue-gray" className="text-[14px] text-black">
             By hitting the "Create Account" button,<br></br> you agree to the
-            <span className='text-purple_two underline mx-[5px]'>
+            <span className="text-purple_two underline mx-[5px]">
               Terms of Service
             </span>
             &<br></br>
-            <span className='text-purple_two underline'>Privacy Policy</span>
+            <span className="text-purple_two underline">Privacy Policy</span>
           </p>
         </div>
 
         <Button
-          size='lg'
-          className='rounded-full bg-primary normal-case text-[15px] mt-2'
-          type='submit'
+          size="lg"
+          className="rounded-full bg-primary normal-case text-[15px] mt-2"
+          type="submit"
           disabled={checked === false}
         >
           Create Account
         </Button>
-        <p className='mt-2 flex items-center justify-center gap-2 text-[14px] text-black'>
+        <p className="mt-2 flex items-center justify-center gap-2 text-[14px] text-black">
           Have an account?
           <span
-            className='text-purple_two underline cursor-pointer'
-            onClick={() => navigate('/sign-in')}
+            className="text-purple_two underline cursor-pointer"
+            onClick={() => navigate("/sign-in")}
           >
             Sign In
           </span>
