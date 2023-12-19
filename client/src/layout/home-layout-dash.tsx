3383bb068ad4eb314142f900b997af8fae9f79ca
diduladishan <diduladishan14@gmail.com>
2023-12-19 14:14:57 +0530

    navigation bar updated

--- client/src/layout/home-layout-dash.tsx
+++ client/src/layout/home-layout-dash.tsx
@@ -1,9 +1,9 @@
-import { Drawer, IconButton } from '@material-tailwind/react';
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import MENU_BAR from '../assets/sidebar/menu.png';
-import { NavbarDashboard } from './NavbarDashboard';
-import SidebarDashboard from './Sidebar_dashboard';
+import { Drawer, IconButton } from "@material-tailwind/react";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import MENU_BAR from "../assets/sidebar/menu.png";
+import { NavbarDashboard } from "./NavbarDashboard";
+import SidebarDashboard from "./Sidebar_dashboard";
 
 const HomeLayout = () => {
   const [open, setOpen] = React.useState(false);
@@ -13,38 +13,42 @@ const HomeLayout = () => {
 
   return (
     <>
-      <main className='w-full h-screen overflow-auto bg-[#f7f6fd]'>
-        <div className=''>
+      <main className="w-full h-screen overflow-auto bg-[#f7f6fd]">
+        <div className="bg-[#957FEF] mb-2">
           <NavbarDashboard />
         </div>
 
-        <div className='block lg:hidden'>
+        <div className="block lg:hidden">
           <React.Fragment>
             {/* <Button onClick={openDrawer}>Open Drawer</Button> */}
             <p onClick={openDrawer}>
-              <img src={MENU_BAR} alt='aad' className='w-[25px] m-2' />
+              <img
+                src={MENU_BAR}
+                alt="aad"
+                className="w-[25px] m-2 fixed top-0 z-50"
+              />
             </p>
-            <Drawer open={open} onClose={closeDrawer} className='p-1 w-min'>
-              <div className='mb-6 flex items-center justify-between'>
+            <Drawer open={open} onClose={closeDrawer} className="p-1 w-min">
+              <div className="mb-6 flex items-center justify-between">
                 <div>
-                  {' '}
+                  {" "}
                   <IconButton
-                    variant='text'
-                    color='blue-gray'
+                    variant="text"
+                    color="blue-gray"
                     onClick={closeDrawer}
                   >
                     <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      fill='none'
-                      viewBox='0 0 24 24'
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
                       strokeWidth={2}
-                      stroke='currentColor'
-                      className='h-5 w-5'
+                      stroke="currentColor"
+                      className="h-5 w-5"
                     >
                       <path
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        d='M6 18L18 6M6 6l12 12'
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
                       />
                     </svg>
                   </IconButton>
@@ -57,8 +61,8 @@ const HomeLayout = () => {
           </React.Fragment>
         </div>
 
-        <div className='flex'>
-          <div className='hidden lg:block'>
+        <div className="flex">
+          <div className="hidden lg:block">
             <SidebarDashboard />
           </div>
 
