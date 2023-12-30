d7820e5e55fec8c312c33db248e0298dfe2ccdd2
diduladishan <diduladishan14@gmail.com>
2023-12-30 20:41:37 +0530

    message name list updated

--- client/src/pages/messages/components/conversation-filter.tsx
+++ client/src/pages/messages/components/conversation-filter.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import REFRESH_ICON from '../../../assets/14_messages/refresh.png';
+import { FC } from "react";
+import REFRESH_ICON from "../../../assets/14_messages/refresh.png";
 
 interface ConversationFilterProps {
   getConversations: () => Promise<void>;
@@ -9,17 +9,17 @@ const ConversationFilter: FC<ConversationFilterProps> = ({
   getConversations,
 }) => {
   return (
-    <div className='border-b border-[#EDECF1] p-4'>
-      <div className='flex items-start justify-between gap-4'>
-        <div className='flex items-center gap-4'>
-          <p className='text-base font-medium text-gray-700'>All</p>
-          <p className='text-base font-medium text-gray-700'>Unread (1)</p>
+    <div className="border-b border-[#EDECF1] p-4">
+      <div className="flex items-start justify-between gap-4">
+        <div className="flex items-center gap-4">
+          <p className="text-base font-medium text-gray-700">All</p>
+          {/* <p className='text-base font-medium text-gray-700'>Unread (1)</p> */}
         </div>
         <button onClick={getConversations}>
           <img
             src={REFRESH_ICON}
-            alt='refresh'
-            className='object-contain w-4 h-4 text-gray-500'
+            alt="refresh"
+            className="object-contain w-4 h-4 text-gray-500"
           />
         </button>
       </div>
