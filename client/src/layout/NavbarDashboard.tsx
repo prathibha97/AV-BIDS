fd46ae12e0b84ac9e264e4735ede81a6455e2150
diduladishan <diduladishan14@gmail.com>
2023-12-30 19:18:53 +0530

    toggle button and sign in UI fixed

--- client/src/layout/NavbarDashboard.tsx
+++ client/src/layout/NavbarDashboard.tsx
@@ -31,7 +31,7 @@ export function NavbarDashboard() {
   }, []);
 
   const navList = (
-    <ul className="mb-4 mt-2 flex justify-center flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="flex justify-center flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <div className="hidden sm:block">
         <Link to="/events">
           <p className="text-white">Events Page</p>
